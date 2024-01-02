0cae009714ee05b877a26f466dd163b23c442555
rmrighes <rmrighes@users.noreply.github.com>
2024-01-01 20:04:32 -0700

    Edits from running a code formatter

--- example.py
+++ example.py
@@ -2,36 +2,36 @@ import asyncio
 from time import perf_counter
 from yajaw import api as jira_api
 
+
 async def main() -> None:
+    time_start = perf_counter()
+    projects = await jira_api.get_all_projects()
+    time_finish = perf_counter()
+    print(f"Total get_all_projects time is {time_finish - time_start} seconds.")
+    search_keys = list()
+    for project in projects:
+        search_keys.append(project["key"])
 
-    #time_start = perf_counter()    
-    #projects = await jira_api.get_all_projects()
-    #time_finish = perf_counter()
-    #print(f"Total get_all_projects time is {time_finish - time_start} seconds.")
-    #search_keys = list()
-    #for project in projects:
-    #    search_keys.append(project["key"])
+    time_start = perf_counter()
+    project = await jira_api.get_project("EMI")
+    time_finish = perf_counter()
+    print(f"Total get_project time is {time_finish - time_start} seconds.")
 
-    #time_start = perf_counter()    
-    #project = await jira_api.get_project("EMI")
-    #time_finish = perf_counter()
-    #print(f"Total get_project time is {time_finish - time_start} seconds.")
+    time_start = perf_counter()
+    projects = await jira_api.get_projects_from_list(search_keys)
+    time_finish = perf_counter()
+    print(f"Total get_projects_from_list time is {time_finish - time_start} seconds.")
+    print(f"There is a total of {len(projects)} projects in the response.")
 
-    #time_start = perf_counter()    
-    #projects = await jira_api.get_projects_from_list(search_keys)
-    #time_finish = perf_counter()
-    #print(f"Total get_projects_from_list time is {time_finish - time_start} seconds.")
-    #print(f"There is a total of {len(projects)} projects in the response.")
-  
-    #jql = f'project = ETOE'
-    #time_start = perf_counter()
-    #response = await jira_api.search_issues_jql(jql)
-    #time_finish = perf_counter()
-    #print(f"Total get_projects_from_list time is {time_finish - time_start} seconds.")
+    jql = f'project = ETOE'
+    time_start = perf_counter()
+    response = await jira_api.search_issues_jql(jql)
+    time_finish = perf_counter()
+    print(f"Total get_projects_from_list time is {time_finish - time_start} seconds.")
 
-    #proj_key = "ETOE"
-    #response = await jira_api.get_issues_from_project(key=proj_key)
-    #print(len(response))
+    proj_key = "ETOE"
+    response = await jira_api.get_issues_from_project(key=proj_key)
+    print(len(response))
 
     time_start = perf_counter()
     proj_key = ["ETOE", "ESTT"]
@@ -40,7 +40,9 @@ async def main() -> None:
         print(f'{issue["fields"]["project"]["name"]} :: {issue["key"]}')
     print(len(response))
     time_finish = perf_counter()
-    print(f"Total get_issues_from_projects is {time_finish - time_start} seconds.")
+    print(f"Total get_issues_from_projects is {
+          time_finish - time_start} seconds.")
+
 
 if __name__ == "__main__":
     raise SystemExit(asyncio.run(main()))
--- src/yajaw/api.py
+++ src/yajaw/api.py
@@ -1,6 +1,7 @@
 import asyncio
 import httpx
-#3.13? from warnings import deprecated
+
+# 3.13? from warnings import deprecated
 from .configuration import *
 
 
@@ -18,7 +19,7 @@ def convert_gathered_responses_to_json(group_resp) -> list[any]:
 
 async def slowdown_if_needed(resp) -> None:
     if int(resp.headers["x-ratelimit-remaining"]) < 25:
-        await asyncio.sleep(0.3) # rate limiting control
+        await asyncio.sleep(0.3)  # rate limiting control
 
 
 def is_success_response(resp) -> bool:
@@ -28,13 +29,14 @@ def is_success_response(resp) -> bool:
 
 
 def rate_limit_reached(attempts: int) -> bool:
-    if attempts >= 10: 
+    if attempts >= 10:
         return True
     return False
 
 
 def retry_request(resp, attempts) -> bool:
-    log_message = f"Status Code: {resp.status_code} -- attempt {attempts:02d} -- {resp.request.method} {resp.request.url}"
+    log_message = f"Status Code: {
+        resp.status_code} -- attempt {attempts:02d} -- {resp.request.method} {resp.request.url}"
     if rate_limit_reached(attempts):
         retry = False
         logger.error(log_message)
@@ -47,7 +49,9 @@ def retry_request(resp, attempts) -> bool:
     return retry
 
 
-def convert_response_obj_to_dict(resp: list[httpx.Response] | httpx.Response) -> list[dict[any]] | dict[any]:
+def convert_response_obj_to_dict(
+    resp: list[httpx.Response] | httpx.Response,
+) -> list[dict[any]] | dict[any]:
     if isinstance(resp, httpx.Response):
         result = resp.json()
     elif isinstance(resp, list):
@@ -57,7 +61,9 @@ def convert_response_obj_to_dict(resp: list[httpx.Response] | httpx.Response) ->
     return result
 
 
-def extract_values_from_key_in_list_of_dict(grouped_response: list[dict[any]], key: str) -> list[any]:
+def extract_values_from_key_in_list_of_dict(
+    grouped_response: list[dict[any]], key: str
+) -> list[any]:
     r = list()
     for single_group in grouped_response:
         for item in single_group[key]:
@@ -71,26 +77,25 @@ def extract_values_from_key_in_list_of_dict(grouped_response: list[dict[any]], k
 
 
 def set_request_headers() -> dict[any]:
-    return { "Accept": "application/json",
-             "Authorization": f"Bearer {JIRA_PAT}"}
+    return {"Accept": "application/json", "Authorization": f"Bearer {JIRA_PAT}"}
 
 
-async def send_request(client: httpx.AsyncClient = None, 
-                       semaphore: asyncio.BoundedSemaphore = None, 
-                       method: str = None, 
-                       url: str = None, 
-                       headers: dict = None,
-                       params: dict = None,
-                       payload: dict = None) -> httpx.Response:
+async def send_request(
+    client: httpx.AsyncClient = None,
+    semaphore: asyncio.BoundedSemaphore = None,
+    method: str = None,
+    url: str = None,
+    headers: dict = None,
+    params: dict = None,
+    payload: dict = None,
+) -> httpx.Response:
     async with semaphore:
         attempts = 0
         retry = True
         while retry:
-            response = await client.request(method=method, 
-                                            url=url, 
-                                            headers=headers,
-                                            params=params,
-                                            json=payload)
+            response = await client.request(
+                method=method, url=url, headers=headers, params=params, json=payload
+            )
             attempts += 1
             # TODO: check if a valid httpx.Response object was returned
             await slowdown_if_needed(response)
@@ -98,30 +103,34 @@ async def send_request(client: httpx.AsyncClient = None,
     return response
 
 
-# TODO: Calls to next page are sequential. 
+# TODO: Calls to next page are sequential.
 # It can be improved to become concurrent at the pagination level after the first call.
-async def send_request_paginated(client: httpx.AsyncClient = None, 
-                                 semaphore: asyncio.BoundedSemaphore = None,
-                                 method: str = None, 
-                                 url: str = None, 
-                                 headers: dict = None,
-                                 params: dict = None, 
-                                 payload: dict = None) -> list[httpx.Response]:
+async def send_request_paginated(
+    client: httpx.AsyncClient = None,
+    semaphore: asyncio.BoundedSemaphore = None,
+    method: str = None,
+    url: str = None,
+    headers: dict = None,
+    params: dict = None,
+    payload: dict = None,
+) -> list[httpx.Response]:
     result = list()
     startAt = payload["startAt"]
     maxResults = payload["maxResults"]
     more_pages = True
     counter = 0
     while more_pages:
-        response = await send_request(client=client,
-                                      semaphore=semaphore,
-                                      method=method,
-                                      url=url,
-                                      headers=headers,
-                                      params=params,
-                                      payload=payload)
+        response = await send_request(
+            client=client,
+            semaphore=semaphore,
+            method=method,
+            url=url,
+            headers=headers,
+            params=params,
+            payload=payload,
+        )
         result.append(response)
-        
+
         counter += 1
         total = response.json()["total"]
 
@@ -132,7 +141,7 @@ async def send_request_paginated(client: httpx.AsyncClient = None,
         else:
             startAt = startAt + maxResults
             payload["startAt"] = startAt
-        
+
     return result
 
 
@@ -141,15 +150,19 @@ async def send_request_paginated(client: httpx.AsyncClient = None,
 #######################################
 
 
-#3.13? @deprecated("Use Get projects paginated that supports search and pagination")
+# 3.13? @deprecated("Use Get projects paginated that supports search and pagination")
 async def get_all_projects() -> list[any]:
     headers = set_request_headers()
     async with httpx.AsyncClient(verify=False) as client:
-        task = asyncio.create_task(send_request(client=client, 
-                                                semaphore=semaphore,
-                                                method="GET",
-                                                url=f"{JIRA_BASE_URL}/{JIRA_API}/project", 
-                                                headers=headers))
+        task = asyncio.create_task(
+            send_request(
+                client=client,
+                semaphore=semaphore,
+                method="GET",
+                url=f"{JIRA_BASE_URL}/{JIRA_API}/project",
+                headers=headers,
+            )
+        )
         responses = await task
     return responses.json()
 
@@ -157,11 +170,15 @@ async def get_all_projects() -> list[any]:
 async def get_project(key=str) -> dict[any]:
     headers = set_request_headers()
     async with httpx.AsyncClient(verify=False) as client:
-        task = asyncio.create_task(send_request(client=client, 
-                                                semaphore=semaphore,
-                                                method="GET",
-                                                url=f"{JIRA_BASE_URL}/{JIRA_API}/project/{key}", 
-                                                headers=headers))
+        task = asyncio.create_task(
+            send_request(
+                client=client,
+                semaphore=semaphore,
+                method="GET",
+                url=f"{JIRA_BASE_URL}/{JIRA_API}/project/{key}",
+                headers=headers,
+            )
+        )
         response = await task
     return response.json()
 
@@ -169,36 +186,46 @@ async def get_project(key=str) -> dict[any]:
 async def get_projects_from_list(keys=list[str]) -> list[any]:
     headers = set_request_headers()
     async with httpx.AsyncClient(verify=False) as client:
-        tasks = [ asyncio.create_task(send_request(client=client, 
-                                                   semaphore=semaphore,
-                                                   method="GET",
-                                                   url=f"{JIRA_BASE_URL}/{JIRA_API}/project/{key}", 
-                                                   headers=headers)) for key in keys]
+        tasks = [
+            asyncio.create_task(
+                send_request(
+                    client=client,
+                    semaphore=semaphore,
+                    method="GET",
+                    url=f"{JIRA_BASE_URL}/{JIRA_API}/project/{key}",
+                    headers=headers,
+                )
+            )
+            for key in keys
+        ]
         group_responses = await asyncio.gather(*tasks)
     return convert_response_obj_to_dict(group_responses)
 
 
 async def search_issues_jql(jql: str = None) -> list[any]:
     headers = set_request_headers()
-    payload = { "jql": f"{jql}", 
-                "startAt": 0, 
-                "maxResults": 50}
+    payload = {"jql": f"{jql}", "startAt": 0, "maxResults": 50}
     async with httpx.AsyncClient(verify=False, timeout=30.0) as client:
         task = asyncio.create_task(
-            send_request_paginated(client=client, 
-                                   semaphore=semaphore,
-                                   method="POST",
-                                   url=f"{JIRA_BASE_URL}/{JIRA_API}/search", 
-                                   headers=headers,
-                                   payload=payload))
+            send_request_paginated(
+                client=client,
+                semaphore=semaphore,
+                method="POST",
+                url=f"{JIRA_BASE_URL}/{JIRA_API}/search",
+                headers=headers,
+                payload=payload,
+            )
+        )
         response = await task
         processed_response = convert_response_obj_to_dict(response)
         issues = list()
-        issues = extract_values_from_key_in_list_of_dict(processed_response, "issues")
+        issues = extract_values_from_key_in_list_of_dict(
+            processed_response, "issues")
         return issues
 
+
 def get_next_project_jql(project_key: str):
-    return f'project = {project_key}'
+    return f"project = {project_key}"
 
 
 def flatten_list_of_dictionaries(input_list: list[list[dict[any]]]) -> list[dict]:
@@ -216,6 +243,10 @@ async def get_issues_from_project(key: str | list[str]) -> list[any]:
         return await search_issues_jql(jql=jql)
     # A list of strings with project keys
     elif isinstance(key, list):
-        tasks = [ asyncio.create_task(search_issues_jql(jql=get_next_project_jql(next_key))) for next_key in key ]
+        tasks = [
+            asyncio.create_task(search_issues_jql(
+                jql=get_next_project_jql(next_key)))
+            for next_key in key
+        ]
         list_responses = await asyncio.gather(*tasks)
         return flatten_list_of_dictionaries(list_responses)
