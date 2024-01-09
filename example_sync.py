3d2e64a58614b44b69a48720eaa6fd0b08b32839
rmrighes <rmrighes@users.noreply.github.com>
2024-01-08 21:17:05 -0700

    Fixed lint errors in the jira.py file

--- example_sync.py
+++ example_sync.py
@@ -6,26 +6,37 @@ def main():
     """Main function used to execute the example."""
 
     print("Resource: project")
-    projects = jira.fetch_all_projects(expand=None)
+    projects = jira.fetch_all_projects(
+        expand={"expand":"description"})
     print(* (project["key"] for project in projects), sep="\n")
     print(f"There is a total of {len(projects)} projects.")
     print()
     print("Resource: projec/{project_key}")
-    project = jira.fetch_project(project_key="ETOE", expand=None)
+    project = jira.fetch_project(
+        project_key="ETOE",
+        expand={"expand":"description"})
     print(project["key"])
     print()
     print("Resource: project/{project_key} from list")
     project_keys = {"ETOE","ESTT"}
-    projects = jira.fetch_projects_from_list(project_keys=project_keys)
+    projects = jira.fetch_projects_from_list(
+        project_keys=project_keys,
+        expand={"expand":"description"})
     print(* (project["key"] for project in projects), sep="\n")
     print()
     print("Resource: issue/{issue_key}")
-    issue = jira.fetch_issue(issue_key="ETOE-8", expand=None)
+    issue = jira.fetch_issue(
+        issue_key="ETOE-8",
+        expand={"expand":"changelog"})
     print(f"{issue["key"]} -- {issue["fields"]["summary"]}")
+    print(f"A total of {issue["changelog"]["total"]} change(s) in the changelog.")
     print()
     print("Resource: search/{jql}")
     jql = "project in (ETOE, EMI, ESTT, MLOPS)"
-    issues = jira.search_issues(jql=jql, field="issues")
+    issues = jira.search_issues(
+        jql=jql,
+        expand={"expand":"description"},
+        field="issues")
     print(* (f"{issue["key"]} - {issue["fields"]["summary"]}" for issue in issues), sep="\n")
     print(f"There is a total of {len(issues)} issues returned by the search.")
     print()
--- src/yajaw/jira.py
+++ src/yajaw/jira.py
@@ -1,3 +1,5 @@
+""" Module responsible for the externally accessible functions wrapping 
+the Jira API."""
 import asyncio
 
 from yajaw.core import rest
@@ -7,37 +9,56 @@ type list_responses = list[dict[any]]
 type single_response = dict[any]
 
 
-async def async_fetch_all_projects(expand: str = None) -> list_responses:
+async def async_fetch_all_projects(expand: dict = None) -> list_responses:
+    """Wrapper async function for GET /project"""
     method = "GET"
     resource = "project"
-    response = await rest.send_single_request(method=method, resource=resource)
+    if expand is None:
+        expand = {}
+    response = await rest.send_single_request(
+        method=method, resource=resource, params=expand
+    )
     projects = conversions.process_multiple_nonpaginated_resources(responses=response)
     return projects
 
 
-def fetch_all_projects(expand: str = None) -> list_responses:
+def fetch_all_projects(expand: dict = None) -> list_responses:
+    """Wrapper sync function for GET /project"""
+    if expand is None:
+        expand = {}
     loop = asyncio.new_event_loop()
     coroutine = async_fetch_all_projects(expand=expand)
     return loop.run_until_complete(coroutine)
 
 
-async def async_fetch_project(project_key: str, expand: str = None) -> single_response:
+async def async_fetch_project(project_key: str, expand: dict = None) -> single_response:
+    """Wrapper async function for GET /project/{key}"""
     method = "GET"
     resource = f"project/{project_key}"
-    response = await rest.send_single_request(method=method, resource=resource)
+    if expand is None:
+        expand = {}
+    response = await rest.send_single_request(
+        method=method, resource=resource, params=expand
+    )
     project = conversions.process_single_nonpaginated_resource(responses=response)
     return project
 
 
-def fetch_project(project_key: str, expand: str = None) -> single_response:
+def fetch_project(project_key: str, expand: dict = None) -> single_response:
+    """Wrapper sync function for GET /project/{key}"""
+    if expand is None:
+        expand = {}
     loop = asyncio.new_event_loop()
     coroutine = async_fetch_project(project_key=project_key, expand=expand)
     return loop.run_until_complete(coroutine)
 
 
 async def async_fetch_projects_from_list(
-    project_keys: list[str], expand: str = None
+    project_keys: list[str], expand: dict = None
 ) -> list_responses:
+    """Wrapper async function for multiple calls on GET /project/{key}"""
+    if expand is None:
+        expand = {}
     tasks = [
         asyncio.create_task(async_fetch_project(project_key=key, expand=expand))
         for key in project_keys
@@ -47,39 +68,51 @@ async def async_fetch_projects_from_list(
 
 
 def fetch_projects_from_list(
-    project_keys: list[str], expand: str = None
+    project_keys: list[str], expand: dict = None
 ) -> list_responses:
+    """Wrapper sync function for multiple calls on GET /project/{key}"""
+    if expand is None:
+        expand = {}
     loop = asyncio.new_event_loop()
     coroutine = async_fetch_projects_from_list(project_keys=project_keys, expand=expand)
     return loop.run_until_complete(coroutine)
 
 
-async def async_fetch_issue(
-    issue_key: str, expand: str = None, fields: str = None
-) -> single_response:
+async def async_fetch_issue(issue_key: str, expand: dict = None) -> single_response:
+    """Wrapper async function for GET /issue/{key}"""
     method = "GET"
     resource = f"issue/{issue_key}"
-    response = await rest.send_single_request(method=method, resource=resource)
+    if expand is None:
+        expand = {}
+    response = await rest.send_single_request(
+        method=method, resource=resource, params=expand
+    )
     issue = conversions.process_single_nonpaginated_resource(responses=response)
     return issue
 
 
-def fetch_issue(issue_key: str, expand: str = None) -> single_response:
+def fetch_issue(issue_key: str, expand: dict = None) -> single_response:
+    """Wrapper sync function for GET /issue/{key}"""
+    if expand is None:
+        expand = {}
     loop = asyncio.new_event_loop()
     coroutine = async_fetch_issue(issue_key=issue_key, expand=expand)
     return loop.run_until_complete(coroutine)
 
 
 async def async_search_issues(
-    jql: str, expand: str = None, field: str = None
+    jql: str, expand: dict = None, field: str = None
 ) -> list_responses:
+    """Wrapper async function for POST /search"""
     method = "POST"
     resource = "search"
+    if expand is None:
+        expand = {}
     content = {"jql": jql}
     field_array = field
     payload = rest.generate_payload(content)
     response = await rest.send_paginated_requests(
-        method=method, resource=resource, payload=payload
+        method=method, resource=resource, params=expand, payload=payload
     )
     issues = conversions.process_multiple_paginated_resources(
         responses=response, field_array=field_array
@@ -87,16 +120,10 @@ async def async_search_issues(
     return issues
 
 
-def search_issues(jql: str, expand: str = None, field: str = None) -> list_responses:
+def search_issues(jql: str, expand: dict = None, field: str = None) -> list_responses:
+    """Wrapper sync function for POST /search"""
+    if expand is None:
+        expand = {}
     loop = asyncio.new_event_loop()
     coroutine = async_search_issues(jql=jql, expand=expand, field=field)
     return loop.run_until_complete(coroutine)
-
-
-# Review under this point
-
-
-def fetch_all_issues_from_projects(
-    project_keys: list[str], expand: str = None, fields: str = None
-) -> list_responses:
-    ...
