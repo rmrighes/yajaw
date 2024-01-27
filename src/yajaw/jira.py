aac843e2483bf7c37cdb0398ba91036ac7575a0e
rmrighes <rmrighes@users.noreply.github.com>
2024-01-26 22:28:36 -0700

    Fixing bug on API selection

--- src/yajaw/jira.py
+++ src/yajaw/jira.py
@@ -31,15 +31,13 @@ async def async_fetch_all_projects(expand: str | None = None) -> list[dict]:
     """
     expand_dict = {} if expand is None else {"expand": expand}
 
-    info = {
-        "method": "GET",
-        "resource": "project",
-        "api": YajawConfig.SERVER_API,
-        "params": expand_dict,
-        "payload": None,
-    }
-
-    jira = rest.JiraInfo(info=info)
+    jira = rest.JiraInfo(
+        method="GET",
+        resource="project",
+        api=YajawConfig.SERVER_API,
+        params=expand_dict,
+        payload=None,
+    )
 
     try:
         response = await rest.send_single_request(jira=jira)
@@ -91,15 +89,13 @@ async def async_fetch_project(project_key: str, expand: str | None = None) -> di
     """
     expand_dict = {} if expand is None else {"expand": expand}
 
-    info = {
-        "method": "GET",
-        "resource": f"project/{project_key}",
-        "api": YajawConfig.SERVER_API,
-        "params": expand_dict,
-        "payload": None,
-    }
-
-    jira = rest.JiraInfo(info=info)
+    jira = rest.JiraInfo(
+        method="GET",
+        resource=f"project/{project_key}",
+        api=YajawConfig.SERVER_API,
+        params=expand_dict,
+        payload=None,
+    )
 
     try:
         response = await rest.send_single_request(jira=jira)
@@ -153,18 +149,16 @@ async def async_fetch_projects_from_list(
     """
     expand_dict = {} if expand is None else {"expand": expand}
 
-    info_list = [
-        {
-            "method": "GET",
-            "resource": f"project/{project_key}",
-            "api": YajawConfig.SERVER_API,
-            "params": expand_dict,
-            "payload": None,
-        }
+    jira_list = [
+        rest.JiraInfo(
+            method="GET",
+            resource=f"project/{project_key}",
+            api=YajawConfig.SERVER_API,
+            params=expand_dict,
+            payload=None,
+        )
         for project_key in project_keys
     ]
-
-    jira_list = [rest.JiraInfo(info=info) for info in info_list]
 
     try:
         tasks = [asyncio.create_task(rest.send_single_request(jira=jira)) for jira in jira_list]
@@ -198,7 +192,7 @@ def fetch_projects_from_list(project_keys: list[str], expand: str | None = None)
 
 
 async def async_fetch_issue(
-    issue_key: str, expand: str | None = None, agile: ApiType = ApiType.CLASSIC
+    issue_key: str, expand: str | None = None, api: ApiType = ApiType.CLASSIC
 ) -> dict:
     """
     Async call to fetch the details of a single issue.
@@ -215,7 +209,7 @@ async def async_fetch_issue(
         list of attributes to be expanded. They are: renderedFields, name, schema,\
         transitions,editmeta, changelog, and versionedRepresentations.\
         Defaults to None.
-        agile (ApiType, optional): Takes the enumeration values ApiType.CLASSIC or\
+        api (ApiType, optional): Takes the enumeration values ApiType.CLASSIC or\
         ApiType.AGILE as possible values to represent which API to be used. Defaults\
         to ApiType.CLASSIC.
 
@@ -225,17 +219,11 @@ async def async_fetch_issue(
     """
     expand_dict = {} if expand is None else {"expand": expand}
 
-    api = YajawConfig.SERVER_API if not agile else YajawConfig.AGILE_API
+    api_value = YajawConfig.SERVER_API if api == ApiType.CLASSIC else YajawConfig.AGILE_API
 
-    info = {
-        "method": "GET",
-        "resource": f"issue/{issue_key}",
-        "api": api,
-        "params": expand_dict,
-        "payload": None,
-    }
-
-    jira = rest.JiraInfo(info=info)
+    jira = rest.JiraInfo(
+        method="GET", resource=f"issue/{issue_key}", api=api_value, params=expand_dict, payload=None
+    )
 
     try:
         response = await rest.send_single_request(jira=jira)
@@ -245,9 +233,7 @@ async def async_fetch_issue(
 
 
 @async_to_sync
-def fetch_issue(
-    issue_key: str, expand: str | None = None, agile: ApiType = ApiType.CLASSIC
-) -> dict:
+def fetch_issue(issue_key: str, expand: str | None = None, api: ApiType = ApiType.CLASSIC) -> dict:
     """
     Sync call to fetch the details of a single issue.
 
@@ -263,7 +249,7 @@ def fetch_issue(
         list of attributes to be expanded. They are: renderedFields, name, schema,\
         transitions,editmeta, changelog, and versionedRepresentations.\
         Defaults to None.
-        agile (ApiType, optional): Takes the enumeration values ApiType.CLASSIC or\
+        api (ApiType, optional): Takes the enumeration values ApiType.CLASSIC or\
         ApiType.AGILE as possible values to represent which API to be used. Defaults\
         to ApiType.CLASSIC.
 
@@ -271,7 +257,7 @@ def fetch_issue(
         Dictionary with the issue details. An empty dictionary is returned\
         if nothing is found.
     """
-    return async_fetch_issue(issue_key=issue_key, expand=expand, agile=agile)
+    return async_fetch_issue(issue_key=issue_key, expand=expand, api=api)
 
 
 async def async_search_issues(jql: str, expand: str | None = None) -> list[dict]:
@@ -296,15 +282,13 @@ async def async_search_issues(jql: str, expand: str | None = None) -> list[dict]
     expand_dict = {} if expand is None else {"expand": expand}
     query = {"jql": jql}
 
-    info = {
-        "method": "POST",
-        "resource": "search",
-        "api": YajawConfig.SERVER_API,
-        "params": expand_dict,
-        "payload": query,
-    }
-
-    jira = rest.JiraInfo(info=info)
+    jira = rest.JiraInfo(
+        method="POST",
+        resource="search",
+        api=YajawConfig.SERVER_API,
+        params=expand_dict,
+        payload=query,
+    )
 
     try:
         responses = await rest.send_paginated_requests(jira=jira)
